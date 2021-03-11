a9070006280b48e06e2d58b150e1a5123f0f1014
pkubaj <pkubaj@users.noreply.github.com>
2021-03-11 15:45:49 +0000

    Fix build on ARM / POWER
    Fixes https://github.com/fubarnetes/rctl/issues/27.

--- src/lib.rs
+++ src/lib.rs
@@ -1827,7 +1827,7 @@ fn rctl_api_wrapper<S: Into<String>>(
 
     loop {
         // Unsafe C call to get the jail resource usage.
-        if unsafe { api(inbuf.as_ptr(), inputlen, outbuf.as_mut_ptr(), outbuf.len()) } != 0 {
+        if unsafe { api(inbuf.as_ptr(), inputlen, outbuf.as_mut_ptr() as *mut libc::c_char, outbuf.len()) } != 0 {
             let err = io::Error::last_os_error();
 
             match err.raw_os_error() {
@@ -1852,7 +1852,7 @@ fn rctl_api_wrapper<S: Into<String>>(
 
         // If everything went well, convert the return C string in the outbuf
         // back into an easily usable Rust string and return.
-        break Ok(unsafe { CStr::from_ptr(outbuf.as_ptr() as *mut i8) }
+        break Ok(unsafe { CStr::from_ptr(outbuf.as_ptr() as *mut libc::c_char) }
             .to_string_lossy()
             .into());
     }
