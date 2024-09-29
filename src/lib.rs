ee1822d2807590b9186998f1d97859705eaeb508
Alan Somers <asomers@gmail.com>
2024-09-29 14:25:30 -0600

    Fix the serde feature after #51
    We should really add a CI configuration that includes that feature.

--- src/lib.rs
+++ src/lib.rs
@@ -107,12 +107,11 @@ pub enum Error {
 /// Helper module containing enums representing [Subjects](Subject)
 mod subject {
     use super::ParseError;
-    use std::fmt;
     use nix::unistd::{self, Uid};
+    use std::fmt;
 
     /// Represents a user subject
     #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-    #[cfg_attr(feature = "serialize", derive(serde::Serialize))]
     pub struct User(pub Uid);
 
     impl User {
@@ -140,6 +139,16 @@ mod subject {
                 Ok(Some(user)) => write!(f, "user:{}", user.name),
                 Ok(None) => write!(f, "user:{}", self.0),
             }
+        }
+    }
+
+    #[cfg(feature = "serialize")]
+    impl serde::Serialize for User {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            self.0.as_raw().serialize(serializer)
         }
     }
 
